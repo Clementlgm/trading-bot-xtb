e959d439b456ea17d513f4e6756efb18f4c68cd0
Clementlgm <117588056+Clementlgm@users.noreply.github.com>
2025-02-11 22:58:06 +0100

    Update bot_cloud.py

--- bot_cloud.py
+++ bot_cloud.py
@@ -153,7 +153,7 @@ class XTBTradingBot:
            
            delta = df['close'].diff()
            gain = (delta.where(delta > 0, 0)).rolling(window=14).mean()
-           loss = (-delta.where(delta < 0, 0)).rolling(window=14).mean()
+           loss = (delta.where(delta < 0, 0)).rolling(window=14).mean()
            rs = gain / loss
            df['RSI'] = 100 - (100 / (1 + rs))
            
